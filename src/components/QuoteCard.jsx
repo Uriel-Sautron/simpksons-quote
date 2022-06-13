b84f521140016ed7bf58d2d5fda00eabf9365ce6
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-06-13 12:15:47 +0200

    Simpsons quote done

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./QuoteCard.css";
+
+export const QuoteCard = ({ quote, character, image }) => (
+  <figure className="QuoteCard">
+    <img src={image} alt={character} />
+    <figcaption>
+      <blockquote>{quote}</blockquote>
+      <cite>{character}</cite>
+    </figcaption>
+  </figure>
+);
